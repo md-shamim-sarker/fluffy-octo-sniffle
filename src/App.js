e0791a14e34ce327eaeda7d6e5f65ef7d3d7425d
shamimspro <shamimspro@gmail.com>
2022-10-09 22:06:52 +0600

    second commit

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import './App.css';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Main from './layouts/Main';
+import Home from './components/Home/Home';
+import Orders from './components/Orders/Orders';
+
+
+function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Main></Main>,
+      children: [
+        {
+          path: '/',
+          loader: () => fetch('tshirts.json'),
+          element: <Home></Home>,
+        },
+        {
+          path: '/orders',
+          element: <Orders></Orders>
+        }
+      ]
+    },
+  ]);
+  return (
+    <div className="App">
+      <RouterProvider router={router} />
+    </div>
+  );
+}
+
+export default App;
